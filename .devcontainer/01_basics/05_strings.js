e34f920086a824749f3865029d8c7bf6ddbf8a1b
Vikash Khatri <102739330+VikashKhatri95@users.noreply.github.com>
2024-06-13 12:31:25 +0000

    String changes

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name="Vikash"
+const roll=1000019398
+// console.log(`My name is ${name} and my roll number is ${roll}`);//use backticks(`) i.e above tab not (')
+const gameNAme=new String('vikash  ')
+// console.log(gameNAme[0]);//Acces index of the string
+// console.log(gameNAme.__proto__);
+// console.log(gameNAme.length);//Count spaces too
+// console.log(gameNAme.toUpperCase());//change to upper
+// console.log(gameNAme.charAt(2));
+// console.log(gameNAme.indexOf("t"));//Gives -1 if not present
+// console.log(gameNAme.indexOf("a"));
+// console.log(gameNAme.substring(0,3))
+const String4="Vikash Khatri"
+// console.log(String4.slice(-8,4));
+// console.log(gameNAme.substring(-5,3))
+const str1="    Vikash"
+// console.log(str1.trim());//trim paces from both sides
+str2=str1.replace("h","")
+// console.log(str2.trim()); 
+
+const url="https://Vikash/1423/webpage"
+// console.log(url.includes("pages"));//Replys boolean
+
+const myname="Vikash Khatri"
+console.log(myname.split(" "));
