3e38745e7fc5618347e4c230107a14d1d383a334
Vikash Khatri <102739330+VikashKhatri95@users.noreply.github.com>
2024-06-13 10:05:49 +0000

    Conversion of all types

---
+++ .devcontainer/01_basics/03_conversioOperation.js
@@ -0,0 +1,20 @@
+let score="33"
+console.log(typeof(score));
+let varInNumber=Number(score)
+console.log(typeof varInNumber);
+
+let name="Alfa3X"
+console.log(typeof(name));//string
+let Checknametype=Number(name);
+console.log(typeof(Checknametype));//number
+console.log(Checknametype);//Typer is NaN
+
+//"33"=>33
+// "33abc"=>NaN
+// true=1 false=0
+
+// is loggedIn=true
+let isloggedin="hitesh"// if empty then false else true
+let chklogged=Boolean(isloggedin)
+console.log(chklogged); //""or null=>false else true
+
