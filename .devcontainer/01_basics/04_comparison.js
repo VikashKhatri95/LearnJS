67094f6e4877bb984f5c3d93b52e77c316b8938c
Vikash Khatri <102739330+VikashKhatri95@users.noreply.github.com>
2024-06-13 10:44:29 +0000

    Comparison changes

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+let num1,num2,num3
+num1=num2=num3=12
+
+let gameCounter=100
+gameCounter++
+// console.log(gameCounter);
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null>=0);//true 0>0 or 0==0
+
+/*
+Note- ==(equality check) and >=,>,<,<=(comparison operators work differently)
+*/
+// console.log("2"==2);//true
+//strict check check without conversion
+// console.log("2"===2);//false
