2a163f78e0535eb0669d47b1b6ae3a3307dbbdd3
Vikash Khatri <102739330+VikashKhatri95@users.noreply.github.com>
2024-06-13 10:23:45 +0000

    More Conversion changes

--- .devcontainer/01_basics/03_conversioOperation.js
+++ .devcontainer/01_basics/03_conversioOperation.js
@@ -1,13 +1,13 @@
 let score="33"
-console.log(typeof(score));
+// console.log(typeof(score));
 let varInNumber=Number(score)
-console.log(typeof varInNumber);
+// console.log(typeof varInNumber);
 
 let name="Alfa3X"
-console.log(typeof(name));//string
+// console.log(typeof(name));//string
 let Checknametype=Number(name);
-console.log(typeof(Checknametype));//number
-console.log(Checknametype);//Typer is NaN
+// console.log(typeof(Checknametype));//number
+// console.log(Checknametype);//Typer is NaN
 
 //"33"=>33
 // "33abc"=>NaN
@@ -16,5 +16,27 @@ console.log(Checknametype);//Typer is NaN
 // is loggedIn=true
 let isloggedin="hitesh"// if empty then false else true
 let chklogged=Boolean(isloggedin)
-console.log(chklogged); //""or null=>false else true
+// console.log(chklogged); //""or null=>false else true
 
+let value=3
+let negvalue=-value
+// console.table([value,negvalue]);
+
+let str1="hello"
+let str2=" Vikash"
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2);
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+// console.log(+true);
+let num1,num2,num3
+num1=num2=num3=12
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
