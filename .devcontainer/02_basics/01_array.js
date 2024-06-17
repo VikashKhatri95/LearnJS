888f9082268ba1a743cfc6ccc6730cecb439827e
Vikash Khatri <102739330+VikashKhatri95@users.noreply.github.com>
2024-06-17 12:33:23 +0000

    array_changes

---
+++ .devcontainer/02_basics/01_array.js
@@ -0,0 +1,26 @@
+//aaray
+const myArr=[0,2,4,5]
+// console.log(myArr[1]);
+/*
+Note-arrays can can more then one type of elemets
+Javascript arrays-copy operations create shallow copies rather than deep copies.
+shallow copies reffeence point
+*/
+const myArr2=new Array(1,2,3,4,5);
+// console.log(myArr2[0]);
+
+//Array Methods
+
+
+// myArr.push(6);//Add a value in the array
+// console.log(myArr);
+
+// myArr.pop()//Delete last item from the array
+// console.log(myArr);
+
+// myArr.unshift(9)//Add item at the begining of the array and shift all the elements to right
+console.log(myArr);
+
+// myArr.shift(9)//Add item at the begining of the array and shift all the elements to right
+console.log(myArr);
+
